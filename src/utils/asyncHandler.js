1a153791f002846a6b050afa7bcb9842e1de2c04
Eccentric Harry <harinathrao13@gmail.com>
2024-06-28 13:46:12 +0530

    feat: Refactor asyncHandler to handle errors and improve readability

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,25 +1,24 @@
-const asyncHandler = (requestHandler) => {
-return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err)=>(
-            next(err)
-        ));
-    }
-}
-
-export { asyncHandler }
-
-// const asyncHandler = () => {}
-// const asyncHandler = (func) => () => {}
-// const asyncHandler = (func) => async () => {}
-
-
-// const asyncHandler = (fn) => async (req, res, next) => {
-//     try {
-//         await fn(req, res, next)
-//     } catch (error) {
-//         res.status(err.code || 500).json({
-//             success: false,
-//             message: err.message
-//         })
-//     }
-// }
+export const asyncHandler = (requestHandler) => {
+    return async (req, res, next) => {
+      try {
+        await Promise.resolve(requestHandler(req, res, next));
+      } catch (error) {
+        const statusCode = error.statusCode || 500;
+        const errorMessage = error.message || "Internal Server Error";
+        res.status(statusCode).json({
+          success: false,
+          error: errorMessage,
+        });
+      }
+    };
+  };
+  // const asyncHandler = (fn) => async (req, res, next) => {
+  //   try {
+  //     await fn(req, res, next);
+  //   } catch (error) {
+  //     res.status(error.code || 500).json({
+  //       success: false,
+  //       message: error.message,
+  //     });
+  //   }
+  // };

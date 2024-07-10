034f2943a4bd872f938678aff5c05020a450499b
H Harinadh <139021195+Eccentric-Harry@users.noreply.github.com>
2024-07-11 05:17:37 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ const allowedOrigins = [
   // process.env.CORS_ORIGIN_PRODUCTION,
   // process.env.CORS_ORIGIN_DEV,
   "https://videotube-frontend.vercel.app",
-  "http://localhost:5173"
+  "http://localhost:5173",
+  "https://videotube-rust.vercel.app"
 ];
 
 // Log allowed origins to verify they are set correctly

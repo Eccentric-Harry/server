974d240a4cb0b6a4bb6f3b9a210ddebc2bef690e
H Harinadh <139021195+Eccentric-Harry@users.noreply.github.com>
2024-07-11 05:52:59 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -30,8 +30,8 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "1gb" }));
+app.use(express.urlencoded({ extended: true, limit: "1gb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
